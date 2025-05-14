acf5af7482b729ad3a59090ce9be2140c5936eb7
Mael <mael.philisot@edu.esiee-it.fr>
2025-05-14 22:11:08 +0200

    Initial commit

---
+++ Ecom/shop/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+"""
+Product
+- Nom 
+- Prix
+- La quantité en stock
+
+"""
+
+class Product(models.Model):
+    name = models.CharField(max_lenght=128, default="test")
+    price = models.FloatField(default=0.0)
+    stock = models.IntegerField(default=0)
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to="products", blank=True, null=True)
