5f3e6fafad88aebcafc53cb30f7b0c610f7b5f80
Mael <mael.philisot@edu.esiee-it.fr>
2025-05-14 22:39:49 +0200

    test setup

--- Ecom/shop/models.py
+++ Ecom/shop/models.py
@@ -11,8 +11,11 @@ Product
 """
 
 class Product(models.Model):
-    name = models.CharField(max_lenght=128, default="test")
+    name = models.CharField(max_length=128, default="test")
     price = models.FloatField(default=0.0)
     stock = models.IntegerField(default=0)
     description = models.TextField(blank=True)
     image = models.ImageField(upload_to="products", blank=True, null=True)
+
+    def __str__(self):
+        return self.name
